5c926970b9d7a46ef04176953488dc8178691a22
Prabhat Soni <52910557+prabhatsn1@users.noreply.github.com>
2024-01-14 11:23:25 +0530

    Add files via upload

---
+++ components/Provider.jsx
@@ -0,0 +1,6 @@
+import { SessionProvider } from "next-auth/react";
+function Provider({ children, session }) {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+}
+
+export default Provider;
